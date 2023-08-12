a0ad1f63324cae76b5dfda40a7448d09d18a321d
jacobv25 <valen306@cougars.csusm.edu>
2023-08-11 23:11:04 -0700

    gitignore and token counter script

--- main.py
+++ main.py
@@ -1,38 +1,18 @@
 import os
-import re
 
-def find_p8_file():
-    for file in os.listdir('.'):
-        if file.endswith('.p8'):
-            return file
-    return None
+# Ensure we're only looking for .lua files in the current directory
+current_directory = os.getcwd()
 
-def read_file(file_path):
-    with open(file_path, 'r') as file:
-        return file.read()
+# Get a list of all .lua files in the current directory
+lua_files = [f for f in os.listdir(current_directory) if os.path.isfile(f) and f.endswith('.lua') and f != 'tokens.lua']
 
-def include_file_content(match):
-    file_path = match.group(1)
-    return read_file(file_path)
+# Open the tokens.lua file for writing
+with open('tokens.lua', 'w') as output_file:
+    for lua_file in lua_files:
+        with open(lua_file, 'r') as file:
+            # Read the content of the current .lua file and write it to tokens.lua
+            content = file.read()
+            output_file.write(f'-- Content from {lua_file} --\n')
+            output_file.write(content + '\n\n')
 
-def parse_and_include_files(p8_file_path):
-    content = read_file(p8_file_path)
-    content = re.sub(r'#include "(.*?)"', include_file_content, content)
-    return content
-
-def create_token_count_file(content):
-    with open('token_count.p8', 'w') as out_file:
-        out_file.write(content)
-
-def main():
-    p8_file_path = find_p8_file()
-    
-    if p8_file_path:
-        print(f'Found p8 file: {p8_file_path}')
-        content = parse_and_include_files(p8_file_path)
-        create_token_count_file(content)
-    else:
-        print('No p8 file found in the current directory.')
-
-if __name__ == "__main__":
-    main()
+print("All .lua files have been merged into tokens.lua!")
